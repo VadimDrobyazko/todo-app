60bad075a4dae27dcf0579e5dc6b65db2f2f68b0
Vadim <volandemortuwka@gmail.com>
2024-05-17 14:10:15 +0300

    Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { UserWarning } from './UserWarning';
+import { USER_ID } from './api/todos';
+import TodoList from './components/TodoList/TodoList';
+import Footer from './components/Footer/Footer';
+import ErrorNotification from './components/Error/ErrorNotification';
+import Header from './components/Header/Header';
+import { useTodos } from './components/Store/Store';
 
 export const App: React.FC = () => {
+  const { filteredTodos, todos } = useTodos();
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+
+        {filteredTodos.length > 0 && <TodoList />}
+
+        {todos.length > 0 && <Footer />}
+      </div>
+
+      <ErrorNotification />
     </div>
   );
 };
